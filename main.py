354f8e0662435885f6b567fe98fad1cdc252ffbf
Barne Jürgensen <barne.juergensen@codeanker.de>
2024-01-02 22:22:15 +0100

    Add SSD1305 OLED display support

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
+import board
+import busio
+import adafruit_ssd1305
 import time
-import Adafruit_SSD1306
 from PIL import Image, ImageDraw, ImageFont
 
 # Create the I2C interface.
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=None)
+i2c = busio.I2C(board.SCL, board.SDA)
 
-# Initialize library.
-disp.begin()
+# Create the SSD1305 OLED class.
+disp = adafruit_ssd1305.SSD1305_I2C(128, 32, i2c)
 
 # Clear display.
-disp.clear()
-disp.display()
+disp.fill(0)
+disp.show()
 
 # Create blank image for drawing.
 width = disp.width
 height = disp.height
-image = Image.new('1', (width, height))
+image = Image.new("1", (width, height))
 
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
 # Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
 # Draw some text.
-font = ImageFont.truetype('./DejaVuSans-Bold.ttf', 10)
+font = ImageFont.load_default()
 text = time.strftime("%H:%M:%S")
 draw.text((0, 0), text, font=font, fill=255)
 
 # Display image.
 disp.image(image)
-disp.display()
+disp.show()
