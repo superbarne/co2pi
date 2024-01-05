89af1501366146c18df19aabee24b8592fb10093
Barne Jürgensen <barne.juergensen@codeanker.de>
2024-01-05 10:43:22 +0100

    Update README.md and main.py

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ python -m pip install -r requirements.txt
 
 
 https://peps.python.org/pep-0008/
+
+
+```/etc/rc.local```
+
+``` bash
+/home/pi/src/co2pi/venv/bin/python /home/pi/src/co2pi/main.py &
+```
--- main.py
+++ main.py
@@ -19,10 +19,6 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create the SSD1305 OLED class.
 disp = adafruit_ssd1305.SSD1305_I2C(128, 32, i2c)
 
-# Clear display.
-disp.fill(0)
-disp.show()
-
 # Create blank image for drawing.
 width = disp.width
 height = disp.height
@@ -31,14 +27,22 @@ image = Image.new("1", (width, height))
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
+while True:
+    # Clear display.
+    disp.fill(0)
+    disp.show()
 
-# Draw some text.
-font = ImageFont.load_default()
-text = "IP: " + get_ip_address()
-draw.text((0, 0), text, font=font, fill=255)
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-# Display image.
-disp.image(image)
-disp.show()
+    # Draw some text.
+    font = ImageFont.load_default()
+    text = "IP: " + get_ip_address()
+    draw.text((0, 0), text, font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.show()
+
+    # Wait for one second.
+    time.sleep(1)
